9156a21c3821acea0aa77fec9beab111fef53c71
andy reyes <11710reyes@gmail.com>
2018-12-23 03:13:19 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,10 @@
 #write your code here
 
-def countdown
+def countdown(numberr)
+  counter = 0 
+  while numberr <= counter
+    puts "#{numberr} SECOND(S)!"
+    counter -= 1
+  end
+  return "HAPPY NEW YEAR!"
 end
